8607a94ae282332c03c4685f162fb23f4b6895c0
Satnam Singh <singhkhowal@gmail.com>
2026-09-19 11:31:57 +0530

    Load compact Groot header styles

--- head.php
+++ head.php
@@ -30,6 +30,8 @@
         <link rel="stylesheet" type="text/css" href="style.css"> <!-- This stylesheet dynamically changed from style.less -->
         <!-- responsive css -->
         <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+        <!-- Groot compact header overrides -->
+        <link rel="stylesheet" type="text/css" href="assets/css/groot-header-compact.css">
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
