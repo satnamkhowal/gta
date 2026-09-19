008c8d6e265316208757e90ff8695806677b477a
shyamdevs <shyamsharma729785@gmail.com>
2026-09-19 16:41:56 +0530

    Auto batch upload 19

---
+++ groot-old-wordpress-website/grootacademy.com/assets2/js/google-firebase-script.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA6Yz_4iw2Skz10sP_waiJpDSNrbAuJ1vQ",
+    authDomain: "groot-academy-digital-learning.firebaseapp.com",
+    projectId: "groot-academy-digital-learning",
+    storageBucket: "groot-academy-digital-learning.appspot.com",
+    messagingSenderId: "1066510015328",
+    appId: "1:1066510015328:web:b20a1cf8e908bc3ae99c05"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
