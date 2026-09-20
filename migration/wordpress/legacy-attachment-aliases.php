78046e7703795ff0744977c6db2029514d2950d6
Satnam Singh <singhkhowal@gmail.com>
2026-09-20 08:06:40 +0530

    Consolidate migration/wordpress/legacy-attachment-aliases.php from groot-academy

---
+++ migration/wordpress/legacy-attachment-aliases.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    '/wp-content/uploads/2024/12/c-programming-course-card-by-groot-academy.png' => '/post/images/card/c-programming-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/cpp-programming-course-card-by-groot-academy.png' => '/post/images/card/cpp-programming-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/core-java-course-card-by-groot-academy.png' => '/post/images/card/core-java-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/advance-java-course-card-by-groot-academy.png' => '/post/images/card/advance-java-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/python-programming-course-card-by-groot-academy.png' => '/post/images/card/python-programming-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/data-science-programming-course-card-by-groot-academy.png' => '/post/images/card/data-science-programming-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/data-science-with-python-course-card-by-groot-academy.png' => '/post/images/card/data-science-with-python-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/data-analysis-with-python-course-card-by-groot-academy.png' => '/post/images/card/data-analysis-with-python-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/web-designing-programming-course-card-by-groot-academy.png' => '/post/images/card/web-designing-programming-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/reactJs-programming-course-card-by-groot-academy.png' => '/post/images/card/reactJs-programming-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/nodeJs-programming-course-card-by-groot-academy.png' => '/post/images/card/nodeJs-programming-course-card-by-groot-academy.png',
+    '/wp-content/uploads/2024/12/php-programming-course-card-by-groot-academy.png' => '/post/images/card/php-programming-course-card-by-groot-academy.png',
+];
